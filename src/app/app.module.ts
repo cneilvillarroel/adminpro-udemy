d6a1a2ed016cca63c2af8a402063122bf516df42
Christopher <cneil.villarroel@gmail.com>
2018-03-07 15:43:55 -0300

    Fin de la seccion 7

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,9 @@ import { APP_ROUTES } from './app.routes';
 import { PagesModule } from './pages/pages.module';
 import { FormsModule } from '@angular/forms';
 
+// Servicios
+import { ServiceModule } from './services/service.module';
+
 // Componentes
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
@@ -24,7 +27,8 @@ import { RegisterComponent } from './login/register.component';
     BrowserModule,
     APP_ROUTES,
     PagesModule,
-    FormsModule
+    FormsModule,
+    ServiceModule
   ],
   providers: [],
   bootstrap: [AppComponent]
